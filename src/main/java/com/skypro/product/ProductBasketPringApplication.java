575c0e00fbda88d65f3c204e71fa1b55c2d105fb
Ann <niyta@mail.ru>
2022-11-20 20:21:10 +0500

    Initial commit

---
+++ src/main/java/com/skypro/product/ProductBasketPringApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductBasketPringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductBasketPringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/product/ProductBasketPringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductBasketPringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
