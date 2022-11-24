beb0183025eb4446076f59e8c87bd32d24a13067
Ann <niyta@mail.ru>
2022-11-24 15:44:43 +0500

    ДЗ выполнено

--- src/main/java/com/skypro/product/controller/ProductController.java
+++ src/main/java/com/skypro/product/controller/ProductController.java
@@ -1,29 +1,26 @@
 package com.skypro.product.controller;
-
-import com.skypro.product.model.Product;
-import com.skypro.product.record.ProductRequest;
-import com.skypro.product.service.Basket;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.skypro.product.service.ProductService;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/store/order")
 public class ProductController {
-    private final Basket basket;
+    private final ProductService productService;
 
-    public ProductController(Basket basket) {this.basket = basket;}
-
-  @GetMapping("/basket")
-    public List<Product> getAllProduct(){
-        return this.basket.getAllProduct();
-  }
-
-  @PostMapping("/basket")
-  public List<Product> addProduct(@RequestBody ProductRequest productRequest){
-        return this.basket.addProduct(productRequest);
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
+
+    @GetMapping("/add")
+    public void addProduct(@RequestParam("id") List<Integer> ids){
+     this.productService.addProduct(ids);
+
+    }
+
+  @GetMapping("/get")
+    public List<Integer> getAllProduct(){
+                return this.productService.getAllProduct();
+  }
 
 }
--- src/main/java/com/skypro/product/record/ProductRequest.java
+++
@@ -1,9 +0,0 @@
-package com.skypro.product.record;
-
-public class ProductRequest {
-    private String barcode;
-
-    public String getBarcode() {return barcode;}
-
-    public void setBarcode(String barcode) {this.barcode = barcode;}
-}
